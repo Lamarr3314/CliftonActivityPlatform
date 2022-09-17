5c0cc87646bf55df416fddcb8395564dacb845d0
Lamarr3314 <lamarrellis6@gmail.com>
2022-09-17 17:19:11 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let container = document.getElementById('EventContainer');
+window.addEventListener("load", function() {
+    let sportsEvent=document.createElement("div");
+    sportsEvent.id="SportsEvent";
+    
+});
